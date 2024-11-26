99901ae95beeb55d6c131e27acc10ea2f9c7f6f6
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-11-26 17:27:09 +0800

    updated ui

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -47,13 +47,13 @@ const Calendar: React.FC = () => {
     <div className="flex justify-between items-center mb-3">
       <button
         onClick={prevMonth}
-        className="flex items-center justify-center hover:bg-[#52796f] text-[#354F52] rounded-full p-2 transition-all duration-300 ml-[30rem]"
+        className="mt-[-6rem] flex items-center justify-center hover:bg-[#52796f] text-[#354F52] rounded-full p-2 transition-all duration-300 ml-[29rem]"
       >
         <ChevronLeft size={24} />
     
       </button>
       <div
-        className="relative font-serif font-bold text-[#354F52] text-3xl cursor-pointer"
+        className="mt-[-6rem] relative font-serif font-bold text-[#354F52] text-4xl cursor-pointer"
         onClick={toggleDropdown}
       >
         <span>{format(currentMonth, 'MMMM yyyy')}</span>
@@ -97,7 +97,7 @@ const Calendar: React.FC = () => {
       </div>
       <button
         onClick={nextMonth}
-        className="flex items-center justify-center  hover:bg-[#52796f] text-[#354F52] rounded-full p-2 transition-all duration-300  mr-[30rem]"
+        className="mt-[-6rem] flex items-center justify-center  hover:bg-[#52796f] text-[#354F52] rounded-full p-2 transition-all duration-300  mr-[29rem]"
       >
         <ChevronRight size={24} />
       </button>
@@ -105,7 +105,7 @@ const Calendar: React.FC = () => {
   );
 
   const renderDaysOfWeek = () => (
-    <div style={{fontFamily: '"Signika Negative", sans-serif' }} className="grid grid-cols-7 text-center font-semibold text-lg mb-2 mt-7">
+    <div className="text-[#354F52] grid grid-cols-7 font-serif text-center font-semibold text-lg mb-2 mt-4">
       {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
         <div key={day} className="p-1">{day}</div>
       ))}
@@ -127,7 +127,7 @@ const Calendar: React.FC = () => {
 
       const dayClasses = `flex flex-col border p-2 h-20 w-full rounded-lg cursor-pointer transition-all duration-300 ${
         isCurrentMonth ? '' : 'text-gray-400'
-      } ${isToday ? 'bg-[#FE9B72] text-black border-[#E5EE91] shadow-lg' : ''}`; // Custom style for today's date
+      } ${isToday ? 'bg-[#FE9B72] text-white border-[#E5EE91] shadow-lg' : ''}`; // Custom style for today's date
 
       dateCells.push(
         <div key={day.toString()} className={dayClasses}>
@@ -144,8 +144,8 @@ const Calendar: React.FC = () => {
     <>
       <WhiteContainer>
         <div>
-          <h1 className="text-[2.5rem] text-[#354F52] font-serif mb-3 ml-6 mt-6">Calendar</h1>
-          <div style={{fontFamily: '"Signika Negative", sans-serif' }} className="p-3 ml-[2rem] max-w-[1340px] mx-auto">
+          <h1 style={{ fontFamily: '"Crimson Pro", serif' }} className="text-[3rem] text-[#354F52] ftracking-normal mb-4 ml-8 mt-7">Calendar</h1>
+          <div style={{ fontFamily: '"Crimson Pro", serif' }} className="text-[3rem] text-[#354F52] ftracking-normal mb-4 ml-8 mt-[1rem]">
             {renderHeader()}
             {renderDaysOfWeek()}
             {renderCells()}
