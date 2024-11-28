4a4c8b6c99dfa81db97aa7f2be22ce9d48c45a31
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2024-11-28 21:40:16 +0800

    restructured the folder content

--- client/src/Routes/ProtectedRoutes.tsx
+++ client/src/Routes/ProtectedRoutes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../context/useAuth";
+import { useAuth } from "../contexts/useAuth";
 
 type Props = { children: React.ReactNode };
 
