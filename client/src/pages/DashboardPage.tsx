80657dc2f0913958457d6f5546745d7a826156ab
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2024-11-30 21:49:36 +0800

    pushing adjusted container

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -15,7 +15,7 @@ const DashboardPage = () => {
           </h1>
           <Avatar/>
             <div>
-              <div className="fixed left-[10.5rem] w-[35rem] h-[21.5rem] bg-white rounded-[1.5rem]">
+              <div className="fixed left-[10.5rem] w-[35rem] h-[21.5rem] bg-white rounded-[1.5rem] top-[6rem]">
                 <h2 className="text-xl font-semibold mb-4">My Tasks</h2>
                 <ul className="space-y-6">
                     <li className="flex justify-between">
@@ -36,8 +36,8 @@ const DashboardPage = () => {
                 </ul>
                 <button className="mt-4 w-[35rem] bg-teal-600 text-white py-2 rounded hover:bg-teal-700">View all</button>
               </div>
-              <div className="fixed bg-white w-[35rem] h-[13rem] top-[30rem] left-[10.5rem] rounded-[1.5rem]">Progress</div>
-              <div className="fixed bg-white w-[45.5rem] h-[36rem] left-[47.5rem] rounded-[1.5rem]">Pets</div>
+              <div className="fixed bg-white w-[35rem] h-[14rem] top-[29rem] left-[10.5rem] rounded-[1.5rem]">Progress</div>
+              <div className="fixed bg-white w-[45.5rem] h-[37rem] left-[47.5rem] rounded-[1.5rem] top-[6rem]">Pets</div>
             </div>
         </WhiteContainer>
       <SideBar />
