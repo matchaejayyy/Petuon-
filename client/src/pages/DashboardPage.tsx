1ee29ecfbdf87f7c616156a757fee64238ddbf30
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-11-30 23:16:19 +0800

    updated

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -11,12 +11,12 @@ const DashboardPage = () => {
     <>
       <WhiteContainer>
           <h1
-             style={{ fontFamily: '"Crimson Pro", serif' }} className="text-[3rem] text-[#354F52] ftracking-normal mb-4 ml-8 mt-7" > Dashboard
+            className="text-[3rem] text-[#354F52] ftracking-normal mb-4 ml-8 mt-7" > Dashboard
           </h1>
           <Avatar/>
             <div>
-              <div className="fixed left-[10.5rem] w-[35rem] h-[21.5rem] bg-white rounded-[1.5rem]">
-                <h2 className="text-xl font-semibold mb-4">My Tasks</h2>
+              <div className="fixed left-[10.5rem] w-[35rem] h-[21.5rem] bg-white rounded-[1.5rem] ">
+                <h2 style={{ fontFamily: '"Signika Negative", sans-serif' }} className="text-xl font-semibold mb-4 p-3">My Tasks</h2>
                 <ul className="space-y-6">
                     <li className="flex justify-between">
                         <span>HOMEWORK 1</span><span className="text-gray-500 text-sm">Today</span>
@@ -36,8 +36,8 @@ const DashboardPage = () => {
                 </ul>
                 <button className="mt-4 w-[35rem] bg-teal-600 text-white py-2 rounded hover:bg-teal-700">View all</button>
               </div>
-              <div className="fixed bg-white w-[35rem] h-[13rem] top-[30rem] left-[10.5rem] rounded-[1.5rem]">Progress</div>
-              <div className="fixed bg-white w-[45.5rem] h-[36rem] left-[47.5rem] rounded-[1.5rem]">Pets</div>
+              <div className="fixed bg-white text-xl w-[35rem] h-[13rem] top-[30rem] left-[10.5rem] rounded-[1.5rem] p-3  ">Progress</div>
+              <div className="fixed bg-white text-xl w-[45.5rem] h-[36rem] left-[47.5rem] rounded-[1.5rem] p-3 ">Pets</div>
             </div>
         </WhiteContainer>
       <SideBar />
