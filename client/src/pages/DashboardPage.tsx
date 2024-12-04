f3d00ec4cc8a3fbebd0d7fa9a5595c2351cbb0d2
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-12-04 23:54:54 +0800

    updated padding in progress text in dashboard

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -16,7 +16,7 @@ const DashboardPage = () => {
               <div className="fixed left-[9.8rem] w-[35rem] h-[21.5rem] bg-white rounded-[1.5rem] top-[6rem] shadow-lg">
                 <ToDoListComponent variant="compact"/>
               </div>
-             <div style={{ fontFamily: '"Signika Negative", sans-serif' }}  className=" font-bold text-[#354F52] fixed bg-white w-[35rem] h-[14rem] top-[29rem] left-[9.8rem] rounded-[1.5rem] text-xl shadow-lg">Progress</div>
+             <div style={{ fontFamily: '"Signika Negative", sans-serif' }}  className=" font-bold text-[#354F52] p-3 fixed bg-white w-[35rem] h-[14rem] top-[29rem] left-[9.8rem] rounded-[1.5rem] text-xl shadow-lg">Progress</div>
               <div style={{ fontFamily: '"Signika Negative", sans-serif' }}  className=" font-bold text-[#354F52]  fixed bg-white w-[45.5rem] h-[37rem] left-[47.5rem] rounded-[1.5rem] top-[6rem] text-xl shadow-lg">
                 <Pets />
               </div>
