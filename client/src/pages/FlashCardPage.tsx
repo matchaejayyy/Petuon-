e86ea3a1dd8a60eca71712fc6208f1d6d8166fcc
flo-yd <floydmatthew.torrechilla-21@cpu.edu.ph>
2024-12-07 13:39:49 +0800

    check if maka push

---
+++ client/src/pages/FlashCardPage.tsx
@@ -0,0 +1,22 @@
+// import WhiteContainer from "../components/WhiteContainer";
+// import Sidebar from "../components/SideBar";
+// import Avatar from "../components/Avatar";
+
+// import FlashcardComponent from "../components/flashcard/FlashCardComponent";
+
+// const FlashCardPage = () => {
+//   return(
+//     <>  
+         
+//         <WhiteContainer>
+//             <h1 style={{ fontFamily: '"Crimson Pro", serif' }} className="text-[3rem] text-[#354F52] ftracking-normal mb-4 ml-1 mt-7">Flashcards</h1>
+//             <FlashcardComponent/>
+//             <Avatar/>
+//         </WhiteContainer>
+//         <Sidebar/>
+
+//     </>
+// )
+// };
+
+// export default FlashCardPage;
