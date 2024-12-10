cd96b1deb1bcab56fa2a3f1d9561198532d6dce7
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-12-10 16:36:12 +0800

    updated

--- client/src/components/NotepadComponent.tsx
+++ client/src/components/NotepadComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import { FilePen, Trash2, FilePlus } from "lucide-react";
 import axios from "axios";
