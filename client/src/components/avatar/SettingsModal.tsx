da9b8bdf84e92e0b812929bb42a4d9b4917fae04
matchaejayyy <elmorjohn.cortez-23@cpu.edu.ph>
2024-12-18 02:21:28 +0800

    updated

--- client/src/components/avatar/SettingsModal.tsx
+++ client/src/components/avatar/SettingsModal.tsx
@@ -7,19 +7,19 @@ interface SettingPageModalProps {
 }
 
 const SettingPageModal: React.FC<SettingPageModalProps> = ({ onClose, fetchUserData }) => {
-  const [password, setPassword] = useState<string>('');
+  const [password, setPassword] = useState<string>(''); 
   const [confirmPassword, setConfirmPassword] = useState<string>(''); // Added confirm password state
-  const [email, setEmail] = useState<string>('');
-  const [username, setUserName] = useState<string>('');
+  const [email, setEmail] = useState<string>(''); 
+  const [username, setUserName] = useState<string>(''); 
   const [initialPassword, setInitialPassword] = useState<string>(''); // Store the initial password
   const token = localStorage.getItem('token');
-  const [passwordMatchError, setPasswordMatchError] = useState<string>('');
+  const [passwordMatchError, setPasswordMatchError] = useState<string>(''); 
   const [emailMatchError, setEmailMatchError] = useState<string>(''); // For password mismatch error
   const [isEditing, setIsEditing] = useState<boolean>(false); // Track whether the user is editing their profile
   const [loading, setLoading] = useState<boolean>(false);
   const [afterLoading, setafterLoading] = useState<boolean>(false);
   const [confirmPW, setConfirmPW] = useState<boolean>(false)
-  const [initialEmail, setInitialEmail] = useState<string>("")
+  const [initialEmail, setInitialEmail] = useState<string>("");
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -35,12 +35,12 @@ const SettingPageModal: React.FC<SettingPageModalProps> = ({ onClose, fetchUserD
         setEmail(userData.user_email);
         setPassword(userData.user_password);
         setInitialPassword(userData.user_password); // Store the initial password
-        setInitialEmail(userData.user_email)
+        setInitialEmail(userData.user_email);
       } catch (error) {
         console.error('Error fetching user data:', error);
       } finally {
-        setafterLoading(true)
-        setLoading(false)
+        setafterLoading(true);
+        setLoading(false);
       }
     };
 
@@ -48,11 +48,7 @@ const SettingPageModal: React.FC<SettingPageModalProps> = ({ onClose, fetchUserD
   }, [token]);
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (password.length === 0 ){
-      setPassword(initialPassword)
-    } else {
-      setPassword(e.target.value);
-    }
+    setPassword(e.target.value); // Update password state on change
     setConfirmPW(true);
   };
 
@@ -124,19 +120,18 @@ const SettingPageModal: React.FC<SettingPageModalProps> = ({ onClose, fetchUserD
           Authorization: `Bearer ${token}`,
         },
       });
-      
+
       setPasswordMatchError(''); // Clear any previous errors
       setEmailMatchError('');
       alert('Profile saved');
       setIsEditing(false);
       setConfirmPW(false);
-      fetchUserData()
+      fetchUserData();
     } catch (err) {
       console.error("Error updating user profile:", err);
       setPasswordMatchError('Failed to update profile. Please try again later.');
     }
   };
-
 
   const handleEditClick = () => {
     setIsEditing(true);
@@ -146,85 +141,82 @@ const SettingPageModal: React.FC<SettingPageModalProps> = ({ onClose, fetchUserD
 
   const handleCancelEdit = () => {
     setConfirmPW(false);
-    setIsEditing(false); 
-    setPassword(initialPassword); 
-    setConfirmPassword(''); 
+    setIsEditing(false);
+    setPassword(initialPassword);
+    setConfirmPassword('');
   };
 
   if (loading) {
-    return (
-      <>
-      </>
-    );
+    return <></>;
   }
 
-if (afterLoading) {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-
-    <div className="w-[25rem] rounded-lg bg-white p-6 shadow-lg" style={{ fontFamily: '"Signika Negative", sans-serif' }}>
-  
-      <h2 className="text-xl font-semibold text-[#354F52]">My Profile</h2>
-  
-      <form className="mt-4" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600" htmlFor="username">
-            Username
-          </label>
-          {isEditing ? (
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUserName(e.target.value)} // Update username state
-              className="w-full rounded border-gray-300 p-2 focus:border-[#354F52] focus:ring focus:ring-[#354F52] placeholder-[#354F52]"
-              placeholder="Enter your username"
-            />
-          ) : (
-            <p className="text-[#354F52]">{username}</p>
-          )}
-        </div>
-  
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600" htmlFor="email">
-            Email
-          </label>
-          {isEditing ? (
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} // Update email state
-              className="w-full rounded border-gray-300 p-2 focus:border-[#354F52] focus:ring focus:ring-[#354F52] placeholder-[#354F52]"
-              placeholder="Enter your email"
-            />
-          ) : (
-            <p className="text-[#354F52]">{email}</p>
-          )}
-        </div>
-        {emailMatchError && isEditing && (
-          <div className="text-red-500 text-sm mb-4">{emailMatchError}</div>
-        )}
-  
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600" htmlFor="password">
-            Password
-          </label>
-          {isEditing ? (
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange} // Update password state
-              className="w-full rounded border-gray-300 p-2 focus:border-[#354F52] focus:ring focus:ring-[#354F52] placeholder-[#354F52]"
-              placeholder="Enter your password"
-            />
-          ) : (
-            <p className="text-[#354F52]">********</p>
+  if (afterLoading) {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="w-[25rem] rounded-lg bg-white p-6 shadow-lg" style={{ fontFamily: '"Signika Negative", sans-serif' }}>
+          <h2 className="text-xl font-semibold text-[#354F52]">My Profile</h2>
+          <form className="mt-4" onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600" htmlFor="username">
+                Username
+              </label>
+              {isEditing ? (
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUserName(e.target.value)} // Update username state
+                  className="w-full rounded border-gray-300 p-2 focus:border-[#354F52] focus:ring focus:ring-[#354F52] placeholder-[#354F52]"
+                  placeholder="Enter your username"
+                />
+              ) : (
+                <p className="text-[#354F52]">{username}</p>
               )}
             </div>
-      
-            {isEditing && confirmPW &&(
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600" htmlFor="email">
+                Email
+              </label>
+              {isEditing ? (
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)} // Update email state
+                  className="w-full rounded border-gray-300 p-2 focus:border-[#354F52] focus:ring focus:ring-[#354F52] placeholder-[#354F52]"
+                  placeholder="Enter your email"
+                />
+              ) : (
+                <p className="text-[#354F52]">{email}</p>
+              )}
+            </div>
+            {emailMatchError && isEditing && (
+              <div className="text-red-500 text-sm mb-4">{emailMatchError}</div>
+            )}
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-600" htmlFor="password">
+                Password
+              </label>
+              {isEditing ? (
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange} // Update password state
+                  className="w-full rounded border-gray-300 p-2 focus:border-[#354F52] focus:ring focus:ring-[#354F52] placeholder-[#354F52]"
+                  placeholder="Enter your password"
+                />
+              ) : (
+                // Only show ***** if the password has content
+                <p className="text-[#354F52]">
+                  {password ? '********' : 'No password set'}
+                </p>
+              )}
+            </div>
+
+            {isEditing && confirmPW && (
               <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-600" htmlFor="confirmPassword">
                   Confirm Password
@@ -239,56 +231,57 @@ if (afterLoading) {
                 />
               </div>
             )}
-      
+
             {passwordMatchError && isEditing && (
               <div className="text-red-500 text-sm mb-4">{passwordMatchError}</div>
             )}
-      
-            {isEditing ? (
+
             <div className="flex justify-between">
-            <button
-              type="button"
-              className="rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
-              onClick={handleCancelEdit}
-            >
-              Cancel Edit
-            </button>
-            <button
-              type="submit"
-              className="rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
-            >
-              Save Changes
-            </button>
-          </div>
+              {isEditing ? (
+                <>
+                  <button
+                    type="button"
+                    className="rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
+                    onClick={handleCancelEdit}
+                  >
+                    Cancel Edit
+                  </button>
+                  <button
+                    type="submit"
+                    className="rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
+                  >
+                    Save Changes
+                  </button>
+                </>
               ) : (
-          <>
-            <button
-              type="button"
-              className="mr-2 rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
-              onClick={onClose}
-            >
-              Exit
-            </button>
-          </>
-        )}
-  
-        {isEditing ? (
-          <>
-          </>
-        ) : (
-          <button
-            type="button"
-            className="rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
-            onClick={handleEditClick}
-          >
-            Edit Profile
-          </button>
-        )}
-      </form>
-    </div>
-  </div>
-  
-  );
+                <>
+                  <button
+                    type="button"
+                    className="mr-2 rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
+                    onClick={onClose}
+                  >
+                    Exit
+                  </button>
+                </>
+              )}
+
+              {isEditing ? (
+                <></>
+              ) : (
+                <button
+                  type="button"
+                  className="rounded bg-[#354F52] px-4 py-2 text-white hover:bg-[#52796F] transition-all duration-300 ease-in-out"
+                  onClick={handleEditClick}
+                >
+                  Edit Profile
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 };
-}
+
 export default SettingPageModal;
